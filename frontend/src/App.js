d6b0e9ffadc5b4a2993ed2a512145754e92d9449
trongdung299 <tngdvr2@gmail.com>
2026-04-13 22:08:08 +0700

    UI

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import VideoSummarizer from "./Ui.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoSummarizer />
+    </div>
+  );
+}
+
+export default App;
